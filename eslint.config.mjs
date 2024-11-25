80e9609c0b12f9b8a222dd71bfa305410731aae9
Priyangshu Sarkar <priyangshusarkar0@gmail.com>
2024-11-25 16:13:27 +0530

    Ready to deplay

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,12 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    ignorePatterns: ["dist/"], // Add this line to ignore the dist folder
-  },
-  { languageOptions: { globals: globals.node } },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
