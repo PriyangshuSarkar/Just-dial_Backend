9d4e4cda60edf3297b3f7b3422cd64ee40c1cfeb
Priyangshu Sarkar <priyangshusarkar0@gmail.com>
2025-01-31 15:35:54 +0530

    Minor Fixes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { json, Express, Request } from "express";
+import express, { json, Express, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { dbConnect } from "./utils/dbConnect";
 import { ApolloServer } from "@apollo/server";
@@ -8,73 +8,75 @@ import { graphqlUploadExpress } from "graphql-upload-ts";
 import { auth } from "./middlewares/auth";
 import morgan from "morgan";
 import cors from "cors";
+import os from "os";
 
 async function startServer() {
-  const app: Express = express();
-  app.use(morgan(process.env.LOG_LEVEL || "combined"));
-  const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
-  app.use(
-    cors({
-      origin: allowedOrigins,
-      credentials: true,
-    })
-  );
-  app.use(cookieParser());
-  app.use(json());
-  app.use(
-    graphqlUploadExpress({
-      maxFileSize:
-        parseInt(process.env.MAX_FILE_SIZE_MB || "100") * 1024 * 1024,
-      maxFiles: parseInt(process.env.MAX_FILES_UPLOAD || "10"),
-    })
-  );
-  app.use(auth);
-  const server = new ApolloServer({
-    typeDefs: schema.typeDefs,
-    resolvers: schema.resolvers,
-    csrfPrevention: process.env.ENABLE_CSRF_PREVENTION === "true",
-    introspection: process.env.GRAPHQL_INTROSPECTION === "true",
-  });
-  await server.start();
-
-  dbConnect();
-
-  // app.use("/api");
-
-  app.use(
-    "/graphql",
-    expressMiddleware(server, {
-      context: async ({ req }) => {
-        const owner = (req as Request).owner;
-        // console.log(owner);
-        return {
-          owner,
-        };
-      },
-    })
-  );
-
-  // *Server Start
-  const port = process.env.PORT;
-
   try {
-    const serverInstance = app.listen(port, () => {
-      const address = serverInstance.address();
-      const actualHost =
-        typeof address === "string"
-          ? address
-          : address?.address === "::"
-          ? "localhost" // Replace IPv6 shorthand with localhost
-          : address?.address;
+    const app: Express = express();
+    app.use(morgan(process.env.LOG_LEVEL || "combined"));
+    const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+    app.use(
+      cors({
+        origin: allowedOrigins,
+        credentials: true,
+      })
+    );
+    app.use(cookieParser());
+    app.use(json());
+    app.use(
+      graphqlUploadExpress({
+        maxFileSize:
+          parseInt(process.env.MAX_FILE_SIZE_MB || "100") * 1024 * 1024,
+        maxFiles: parseInt(process.env.MAX_FILES_UPLOAD || "10"),
+      })
+    );
+    app.use(auth);
+    const server = new ApolloServer({
+      typeDefs: schema.typeDefs,
+      resolvers: schema.resolvers,
+      csrfPrevention: process.env.ENABLE_CSRF_PREVENTION === "true",
+      introspection: process.env.GRAPHQL_INTROSPECTION === "true",
+    });
+    await server.start();
 
-      const actualPort = typeof address === "string" ? port : address?.port;
+    dbConnect();
 
-      // Determine protocol dynamically
-      const protocol = process.env.PROTOCOL === "production" ? "https" : "http";
+    app.get("/", (_: unknown, res: Response) => {
+      res.send("Server is working!");
+    });
 
-      console.log(
-        `🚀 Server ready at ${protocol}://${actualHost}:${actualPort}`
-      );
+    app.use(
+      "/graphql",
+      expressMiddleware(server, {
+        context: async ({ req }) => {
+          const owner = (req as Request).owner;
+          // console.log(owner);
+          return {
+            owner,
+          };
+        },
+      })
+    );
+
+    // *Server Start
+    const port = process.env.PORT;
+
+    app.listen(port, () => {
+      const networkInterfaces = os.networkInterfaces();
+      const addresses: string[] = [];
+
+      Object.keys(networkInterfaces).forEach((interfaceName) => {
+        networkInterfaces[interfaceName]?.forEach((interfaceInfo: any) => {
+          if (interfaceInfo.family === "IPv4" && !interfaceInfo.internal) {
+            addresses.push(interfaceInfo.address);
+          }
+        });
+      });
+
+      console.log("🚀 Server running at:");
+      addresses.forEach((address) => {
+        console.log(`http://${address}:${port}`);
+      });
     });
   } catch (error) {
     console.error(`Server failed to start with the error:\n${error}`);
